596ef368108765879d19a8402e92d1d7e177dbd6
Hugo Delautre <97909616+Gautatyr@users.noreply.github.com>
2023-03-31 10:31:14 +0200

    Add project files.

---
+++ FlashCards/Menus.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCards;
+
+public static class Menus
+{
+    public static void MainMenu(string message = "")
+    {
+        Console.Clear();
+
+        Console.WriteLine($"\n{message}");
+
+        Console.WriteLine("\nMAIN MENU\n");
+        Console.WriteLine("- Type 1 to access the Stacks menu");
+        Console.WriteLine("- Type 2 to access the Study Sessions menu");
+        Console.WriteLine("- Type 0 to close the application");
+        
+        string userInput = Console.ReadLine();  // Need improvments
+
+        switch(userInput)
+        {
+            case "0":
+                Environment.Exit(0);
+                break;
+            case "1":
+                StacksMenu();
+                break;
+            case "2":
+                StudySessionsMenu();
+                break;
+            default:
+                // DisplayError("Wrong input ! Please type a number between 0 and 2")
+                MainMenu("Wrong input ! Please type a number between 0 and 2");
+                break;
+        }
+    }
+
+    private static void StacksMenu()
+    {
+
+    }
+
+    private static void StudySessionsMenu()
+    {
+
+    }
+}
---
+++ FlashCards/Program.cs
@@ -0,0 +1,7 @@
+﻿using static FlashCards.Menus;
+// First the app should create a database if there is none
+// Then create the tables Stacks and FlashCards if they don't exist
+
+/*Code goes here*/
+
+MainMenu("Welcome to the flashcard app !");
